cd6ac9fa64446632f9d1e4850026f962234e1e21
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-01-16 17:39:44 +0200

    completing the movie class

--- app/src/main/java/com/example/mvstudio/Movies.java
+++ app/src/main/java/com/example/mvstudio/Movies.java
@@ -1,4 +1,25 @@
 package com.example.mvstudio;
 
 public class Movies {
+    String name;
+    String category;
+    String detail;
+    public Movies(){}
+    public Movies(String name, String category, String detail) {
+        this.name = name;
+        this.category = category;
+        this.detail = detail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
 }
