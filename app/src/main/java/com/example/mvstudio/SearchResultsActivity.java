a28f8a14b3968957c2b2acbca33d1c685bdef14e
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-01-17 19:42:43 +0200

    search activity

--- app/src/main/java/com/example/mvstudio/SearchResultsActivity.java
+++ app/src/main/java/com/example/mvstudio/SearchResultsActivity.java
@@ -11,9 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
@@ -73,5 +76,7 @@ import butterknife.ButterKnife;
 
             }
         });
+
+        }
     }
-}
+
--- app/src/main/java/com/example/mvstudio/WatchListActivity.java
+++ app/src/main/java/com/example/mvstudio/WatchListActivity.java
@@ -6,14 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+//import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
@@ -29,10 +34,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WatchListActivity extends AppCompatActivity {
-    @BindView(R.id.EditTextSearch) EditText EditTextSearch;
-    @BindView(R.id.imageButton) ImageButton imageButton;
-    @BindView(R.id.listview) ListView myListView;
+   // @BindView(R.id.EditTextSearch) EditText EditTextSearch;
+    //@BindView(R.id.imageButton) ImageButton imageButton;
+    @BindView(R.id.rv) RecyclerView mResultList;
+    @BindView(R.id.searchView) SearchView searchView;
+   // @BindView(R.id.listview) ListView myListView;
+
+
     List<Movies> moviesList;
+    ArrayList<Movies> list;
+    RecyclerView recyclerView;
 
     DatabaseReference moviesdb;
 
@@ -95,7 +106,7 @@ public class WatchListActivity extends AppCompatActivity {
        // moviecategoryTextView.setText("Here are all the Category: " + movieCategory);
       //  moviedetailTextView.setText("Here are all the Detail: " + movieDetail);
 
-        moviesdb.addValueEventListener(new ValueEventListener() {
+       /* moviesdb.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 moviesList.clear();
@@ -113,5 +124,70 @@ public class WatchListActivity extends AppCompatActivity {
 
             }
         });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*/
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(moviesdb != null){
+            moviesdb.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        list = new ArrayList<>();
+                        for (DataSnapshot studentDatasnap : dataSnapshot.getChildren()){
+                            list.add(studentDatasnap.getValue(Movies.class));
+                        }
+                        AdapterClass adapterClass = new AdapterClass(list);
+                        recyclerView.setAdapter(adapterClass);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(WatchListActivity.this, error.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+        if(searchView != null)
+        {
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    search(s);
+                    return false;
+
+                }
+            });
+        }
+
+    }
+
+    private void search(String str){
+        ArrayList<Movies> myList = new ArrayList<>();
+        for(Movies object : list)
+        {
+            if(object.getCategory().toLowerCase().contains(str.toLowerCase())){
+                myList.add(object);
+            }
+        }
+        AdapterClass adapterClass = new AdapterClass(myList);
+        recyclerView.setAdapter(adapterClass);
+
+    }
+
+
+
 }
