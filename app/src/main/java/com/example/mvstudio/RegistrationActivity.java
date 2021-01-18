19a2c5a2150b7ba2b8ea8520fcb492a14db6a975
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-01-18 05:45:35 +0200

    registration

--- app/src/main/java/com/example/mvstudio/RegistrationActivity.java
+++ app/src/main/java/com/example/mvstudio/RegistrationActivity.java
@@ -1,13 +1,24 @@
 package com.example.mvstudio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    final String TAG = "RegistrationActivity" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +26,21 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         mAuth = FirebaseAuth.getInstance();
 
+        Button registerButton = (Button) findViewById(R.id.buttonRegister);
+        final EditText usernameInput = (EditText) findViewById(R.id.usernameInput);
+        final EditText passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup(usernameInput.getText().toString(), passwordInput.getText().toString());
+
+            }
+        });
+
     }
 
-    private  void signup(){
+    private  void signup(String email, String password){
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -26,13 +49,18 @@ public class RegistrationActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+
+                            Toast.makeText(RegistrationActivity.this, "Authentication Successful."+ user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+
+                            finish();
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            Toast.makeText(RegistrationActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+
                         }
 
                         // ...
