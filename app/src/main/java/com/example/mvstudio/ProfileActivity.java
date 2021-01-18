1aeed5fd69d504a129d1bb06867f30458661e120
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-01-18 07:20:31 +0200

    profile activity

---
+++ app/src/main/java/com/example/mvstudio/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.mvstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+    }
+}
